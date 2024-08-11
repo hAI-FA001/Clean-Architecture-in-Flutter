70f38158027ca9f45d13a6b88c1788b89b468aa9
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-11 22:34:46 +0500

    article tile widget

---
+++ lib/features/daily_news/presentation/widgets/article_tile.dart
@@ -0,0 +1,159 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:project_learn_clean_arch/features/daily_news/domain/entities/article.dart';
+
+class ArticleWidget extends StatelessWidget {
+  final ArticleEntity? article;
+  final bool? isRemovable;
+  final void Function(ArticleEntity article)? onRemove;
+  final void Function(ArticleEntity article)? onArticlePressed;
+
+  const ArticleWidget({
+    super.key,
+    this.article,
+    this.onArticlePressed,
+    this.isRemovable,
+    this.onRemove,
+  });
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: _onTap,
+        child: Container(
+          padding: const EdgeInsetsDirectional.only(
+              start: 14, end: 14, bottom: 7, top: 7),
+          height: MediaQuery.of(context).size.width / 2.2,
+          child: Row(
+            children: [
+              _buildImage(context),
+              _buildTitleAndDescription(),
+              _buildRemovableArea(),
+            ],
+          ),
+        ),
+      );
+
+  _buildImage(BuildContext context) => CachedNetworkImage(
+        imageUrl: article!.urlToImage!,
+        imageBuilder: (context, imageProvider) => Padding(
+          padding: const EdgeInsetsDirectional.only(end: 14),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 3,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.08),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+        progressIndicatorBuilder: (context, url, downloadProgress) => Padding(
+          padding: const EdgeInsetsDirectional.only(end: 14),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 3,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.08),
+              ),
+              child: const CupertinoActivityIndicator(),
+            ),
+          ),
+        ),
+        errorWidget: (context, url, error) => Padding(
+          padding: const EdgeInsetsDirectional.only(end: 14),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 3,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.08),
+              ),
+              child: const Icon(Icons.error),
+            ),
+          ),
+        ),
+      );
+
+  _buildTitleAndDescription() => Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 7),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                article!.title ?? '',
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontFamily: 'Butler',
+                  fontWeight: FontWeight.w900,
+                  fontSize: 18,
+                  color: Colors.black87,
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Text(
+                    article!.description ?? '',
+                    maxLines: 2,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.timeline_outlined,
+                    size: 16,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    article!.publishedAt!,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+
+  _buildRemovableArea() => isRemovable!
+      ? GestureDetector(
+          onTap: _onRemove,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Icon(
+              Icons.remove_circle_outline,
+              color: Colors.red,
+            ),
+          ),
+        )
+      : Container();
+
+  _onTap() {
+    if (onArticlePressed != null) {
+      onArticlePressed!(article!);
+    }
+  }
+
+  _onRemove() {
+    if (onRemove != null) {
+      onRemove!(article!);
+    }
+  }
+}
